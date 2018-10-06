a6302defdbb2fd8df4f7da044448aef03faabb9a
annychang1111 <30671162+annychang1111@users.noreply.github.com>
2018-10-06 11:53:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+var acc = document.getElementById("acceleration");
+var locat = document.getElementById("location");
+
 if (isMobile) {
 	var mobile = document.getElementById("mobile");
 	mobile.innerHTML = "You are using a mobile browser.";
+}
+
+if (isMobile) {
+	window.addEventListener('devicemotion', function(event) {
+		acc.innerHTML("acceleration: " + event.acceleration.x + ' m/s2' + "(" + event + ")");
+  		// console.log(event.acceleration.x + ' m/s2');
+	});
 }
